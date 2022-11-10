d67af5112cbf9916a4d555f45e90c197bae18b6c
David Acho <42592415+MartexCorp@users.noreply.github.com>
2022-11-10 18:04:32 +0100

    Updated

--- src/controllers/activation/activation_functions.ts
+++ src/controllers/activation/activation_functions.ts
@@ -13,7 +13,7 @@ export const activateOffer = function(request: Request, response: Response, next
     if(result["resultCode"]==405000000){
       console.log("✔ Offer Subscription Successful at CRM");
       addCustomerMwareTV(_subscriber).then(async (result)=>{
-        await sendAuthSMSToUserPhone(_subscriber,"[Auth]: login:"+result["loginid"]+"\n"+"pass:"+result["password"]+"\n"+"Do not share this code with anyone else!");
+        await sendAuthSMSToUserPhone(_subscriber,"[Auth]: login:"+result["id"]+"\n"+"pass:"+result["pass"]+"\n"+"Do not share this code with anyone else!");
         console.log("✔ Offer Subscription Successful on MWareTV");
         console.log(result);
       });
@@ -87,6 +87,7 @@ async function addCustomerMwareTV(telephoneNumber):Promise<object>{
         // console.log(JSON.stringify(response.data));
         const credentialsJSON = JSON.parse(response.data.toString().replace(/\\/g, ""));
         console.log(credentialsJSON["loginid"])
+        console.log(credentialsJSON["password"])
         resolve({id: credentialsJSON["loginid"],pass: credentialsJSON["password"]});
       })
       .catch(function (error) {
