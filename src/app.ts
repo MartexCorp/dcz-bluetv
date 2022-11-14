c97e54d82f7b534f6b4018e19b51f88289412dde
David Acho <42592415+MartexCorp@users.noreply.github.com>
2022-11-14 10:10:01 +0100

    Updated

--- src/app.ts
+++ src/app.ts
@@ -56,5 +56,5 @@ router.use((req, res, next) => {
 
 /** Server */
 const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT ?? 8080;
+const PORT: any = process.env.PORT ?? 9173;
 httpServer.listen(PORT, () => console.log("The server is running on port " + `${PORT}`));
