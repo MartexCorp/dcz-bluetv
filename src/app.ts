b4b2ae35fc6da46419248100e45ef5a9b94efa7f
David Acho <42592415+MartexCorp@users.noreply.github.com>
2022-11-14 11:17:04 +0100

    Updated

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,19 @@ import sqlite3 from "sqlite3";
 const cors = require("cors");
 
 
-const router: Express = express();
+const app: Express = express();
 /** Use CORS **/
-const corsOptions ={
-  origin: ["http://localhost:8080", "http://bluetv.camtel.cm"],
-  optionSuccessStatus:200
-}
-router.use(cors(corsOptions));
+// app.use(cors());
 
 /** Parse the request */
-router.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 /** Takes care of JSON data */
-router.use(express.json());
+app.use(express.json());
 
 // @ts-ignore
 // @ts-ignore
 /** RULES OF OUR API */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
   // set the CORS policy
   res.header("Access-Control-Allow-Origin", "*");
   // set the CORS headers
@@ -30,6 +26,8 @@ router.use((req, res, next) => {
   // set the CORS method headers
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With,Content-Type,Accept, Authorization");
     return res.status(200).json({});
   }
   next();
@@ -43,10 +41,12 @@ open({
 });
 
 /** Routes */
-router.use("/", routes);
+app.use("/", routes);
+
+app.use(cors());
 
 /** Error handling */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
   const error = new Error("not found");
   return res.status(404).json({
     message: error.message
@@ -54,6 +54,6 @@ router.use((req, res, next) => {
 });
 
 /** Server */
-const httpServer = http.createServer(router);
+const httpServer = http.createServer(app);
 const PORT: any = process.env.PORT ?? 9173;
 httpServer.listen(PORT, () => console.log("The server is running on port " + `${PORT}`));
